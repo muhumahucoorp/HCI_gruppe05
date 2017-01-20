80c2cbdbc4bcaa3b6c6782901f6f40963a76e714
Lu3c1 1 <Lu3c1 1>
2017-01-20 11:00:22 +0100

    States anhand der Pfeile selektierbar. Aktueller state wird durch rot umrandeten Pfeil dargestellt

--- gui.js
+++ gui.js
@@ -46,7 +46,7 @@ function init() {
 	cur_state = "Service";
 	car_type = "";
 	car_mode = "";
-	document.getElementById("taskbar").innerHTML = getArrowHTML(states);
+	document.getElementById("taskbar").innerHTML = getArrowHTML(states, true);
 	document.getElementsByClassName('main')[0].innerHTML = document.getElementById(cur_state).innerHTML;
 }
 
@@ -91,13 +91,21 @@ function advanceState() {
 			break;
 	}
 	
-	states.push(cur_state);
+	if(states.indexOf(cur_state) == -1) {
+		states.push(cur_state);
+	} else {
+		states = states.splice(0, states.indexOf(cur_state) + 1);
+	}
 	
     // update the taskbar
 	document.getElementById("taskbar").innerHTML = "";
 	for (let s of states) {
 		if(s != "Endübersicht") {
-			document.getElementById("taskbar").innerHTML += getArrowHTML(s);
+			if(states.indexOf(s) == states.length-1){
+				document.getElementById("taskbar").innerHTML += getArrowHTML(s, true);
+			} else {
+				document.getElementById("taskbar").innerHTML += getArrowHTML(s, false);
+			}
 		}
 	}
 	
@@ -108,11 +116,62 @@ function advanceState() {
 	}
 }
 
+function selectState(new_state) {
+	cur_state = new_state;
+	switch(cur_state) {
+		case "Service" :
+			document.getElementById("back").style.visibility = "hidden";
+			break;
+		case "Ort und Zeit" :
+			document.getElementById("back").style.visibility = "visible";
+			break;
+		case "Fahrzeugtyp" :
+			document.getElementById("back").style.visibility = "visible";
+			break;
+		case "Fahrzeugwahl" :
+			document.getElementById("back").style.visibility = "visible";
+			break;
+		case "Zusatzfeatures" :
+			document.getElementById("back").style.visibility = "visible";
+			features = ["Discokugel 0€/h"];
+			document.getElementById("ordered_features").innerHTML = "<p style='font-family:Arial,sans-serif; font-size:18px; margin-left:5px;text-decoration:underline;'>Ausgewählte Zusatzfeatures</p> Discokugel 0€/h";
+			break;
+		case "Bestellübersicht" :
+			document.getElementById("back").style.visibility = "visible";
+			break;
+		case "Endübersicht" :
+			var cur_features = "";
+			for(let f of features) {
+				cur_features += f + "<br>";
+			}
+			document.getElementById("extras_text").innerHTML = cur_features;
+			document.getElementById("back").style.visibility = "hidden";
+			break;
+	}
+	
+	document.getElementById("taskbar").innerHTML = "";
+	for (let s of states) {
+		if(s != "Endübersicht") {
+			if(s == cur_state){
+				document.getElementById("taskbar").innerHTML += getArrowHTML(s, true);
+			} else {
+				document.getElementById("taskbar").innerHTML += getArrowHTML(s, false);
+			}
+		}
+	}
+	
+	if(cur_state == "Fahrzeugwahl") {
+			document.getElementsByClassName('main')[0].innerHTML = document.getElementById(car_type + car_mode).innerHTML;
+	} else {
+		document.getElementsByClassName('main')[0].innerHTML = document.getElementById(cur_state).innerHTML;
+	}
+}
+
 function resignState() {
-		 states.splice(states.length - 1, 1);
-		 
-		 cur_state = states[states.length -  1];
-		 
+	 states.splice(states.length - 1, 1);
+	 
+	 cur_state = states[states.length -  1];
+	 
 	switch(cur_state) {
 		case "Service" :
 			document.getElementById("back").style.visibility = "hidden";
@@ -135,7 +194,11 @@ function resignState() {
 	document.getElementById("taskbar").innerHTML = "";
 	for (let s of states) {
 		if(s != "Endübersicht") {
-			document.getElementById("taskbar").innerHTML += getArrowHTML(s);
+			if(states.indexOf(s) == states.length-1){
+				document.getElementById("taskbar").innerHTML += getArrowHTML(s, true);
+			} else {
+				document.getElementById("taskbar").innerHTML += getArrowHTML(s, false);
+			}
 		}
 	}
 	
@@ -148,10 +211,14 @@ function resignState() {
 	}
 }
 
-function getArrowHTML(new_state) {
+function getArrowHTML(new_state, selected) {
 		var arrow_html = "<img src=\"arrows\\";
 		arrow_html += new_state;
-		arrow_html += ".png\" class=\"arrow\"> ";
+		arrow_html += ".png\" class=\"arrow\" onclick=\"selectState(\'" + new_state + "\')\"";
+		if(selected == true) {
+			arrow_html += " style=\'border-style: solid; border-color: red;\'";
+		}
+		arrow_html += ">";
 		return arrow_html;
 	}
 
